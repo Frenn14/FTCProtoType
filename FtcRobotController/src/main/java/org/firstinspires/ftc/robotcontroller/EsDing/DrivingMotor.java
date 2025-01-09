776855d8ae6df2bc57648614097d119ab3ffe4b3
Frenn <implan9102@gmail.com>
2025-01-09 21:10:15 +0900

    add position

--- FtcRobotController/src/main/java/org/firstinspires/ftc/robotcontroller/EsDing/DrivingMotor.java
+++ FtcRobotController/src/main/java/org/firstinspires/ftc/robotcontroller/EsDing/DrivingMotor.java
@@ -9,7 +9,7 @@ public class DrivingMotor extends OpMode {
     DcMotor leftBackMotor;      //Driving base left Back motor
     DcMotor rightFrontMotor;    //Driving base right front motor
     DcMotor rightBackMotor;     //Driving base right back motor
-    double  Speed;           //Driving base movement max speed
+    double  Speed;              //Driving base movement max speed
 
     public DrivingMotor(String leftFrontMotor, String leftBackMotor, String rightFrontMotor, String rightBackMotor, double Speed) {
 
@@ -20,43 +20,84 @@ public class DrivingMotor extends OpMode {
         this.rightBackMotor     = hardwareMap.get(DcMotor.class, rightBackMotor);
 
         // Maximum Speed Settings (Assume a configurable range
-        this.Speed           = Speed;
+        this.Speed              = Speed;
     }
 
     public void moveForward(){
-
+        leftFrontMotor  .setPower(1.0);
+        leftBackMotor   .setPower(1.0);
+        rightFrontMotor .setPower(1.0);
+        rightBackMotor  .setPower(1.0);
     }
 
     public void moveBack(){
-
+        leftFrontMotor  .setPower(-1.0);
+        leftBackMotor   .setPower(-1.0);
+        rightFrontMotor .setPower(-1.0);
+        rightBackMotor  .setPower(-1.0);
     }
 
     public void moveLeft(){
-
+        leftFrontMotor  .setPower(-1.0);
+        leftBackMotor   .setPower(1.0);
+        rightFrontMotor .setPower(1.0);
+        rightBackMotor  .setPower(-1.0);
     }
 
     public void moveRight(){
-
+        leftFrontMotor  .setPower(1.0);
+        leftBackMotor   .setPower(-1.0);
+        rightFrontMotor .setPower(-1.0);
+        rightBackMotor  .setPower(1.0);
     }
 
     public void moveLeftForward(){
-
+        leftFrontMotor  .setPower(0.0);
+        leftBackMotor   .setPower(1.0);
+        rightFrontMotor .setPower(1.0);
+        rightBackMotor  .setPower(0.0);
     }
 
     public void moveLeftBack(){
-
+        leftFrontMotor  .setPower(-1.0);
+        leftBackMotor   .setPower(0.0);
+        rightFrontMotor .setPower(0.0);
+        rightBackMotor  .setPower(-1.0);
     }
 
     public void moveRightForward(){
-
+        leftFrontMotor  .setPower(1.0);
+        leftBackMotor   .setPower(0.0);
+        rightFrontMotor .setPower(0.0);
+        rightBackMotor  .setPower(1.0);
     }
 
     public void moveRightBack(){
+        leftFrontMotor  .setPower(0.0);
+        leftBackMotor   .setPower(-1.0);
+        rightFrontMotor .setPower(-1.0);
+        rightBackMotor  .setPower(0.0);
+    }
 
+    public void moveLeftTurn(){
+        leftFrontMotor  .setPower(-1.0);
+        leftBackMotor   .setPower(-1.0);
+        rightFrontMotor .setPower(1.0);
+        rightBackMotor  .setPower(1.0);
+    }
+
+    public void moveRightTurn(){
+        leftFrontMotor  .setPower(1.0);
+        leftBackMotor   .setPower(1.0);
+        rightFrontMotor .setPower(-1.0);
+        rightBackMotor  .setPower(-1.0);
     }
 
     public void moveStop(){
-
+        leftFrontMotor  .setPower(0.0);
+        leftBackMotor   .setPower(0.0);
+        rightFrontMotor .setPower(0.0);
+        rightBackMotor  .setPower(0.0);
     }
 
     @Override
