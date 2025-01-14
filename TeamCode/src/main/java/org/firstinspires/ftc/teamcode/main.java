0c69d2776b502cad3fae9378da5a0f91749e824b
Frenn <implan9102@gmail.com>
2025-01-14 18:08:02 +0900

    EsDingDrivingMotor1.0v

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/main.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/main.java
@@ -2,7 +2,6 @@ package org.firstinspires.ftc.teamcode;
 
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
-import com.qualcomm.robotcore.util.ElapsedTime;
 
 @TeleOp(name = "main", group = "FTC")
 public class main extends OpMode {
@@ -10,7 +9,7 @@ public class main extends OpMode {
 
     @Override
     public void init() {
-
+        DrivingMotor a = new DrivingMotor("A","B","C","D", 1.0, true);
     }
 
     @Override
